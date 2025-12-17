4ca9e6f6f01e19ec7580496d71249ca69a8f6598
bala-amypo <bala@amypo.in>
2025-12-17 09:48:24 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/LocationServiceImpl.java
@@ -1,14 +1,50 @@
-package com.example.demo.service;
+package com.example.demo.entity;
 
-public class LocationServiceImpl implements LocationService{
-    @autowired
-    LocationRepository Locationrepo;
-    @override
-    public LocationEntity createlocation(LocationEntity le){
-        return Locationrepo.save(le);
-        public List<LocationEntity> getall(){
-            return Locationrepo.findAll();
-        }
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class LocationEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private long latitude;
+    private long longitude;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public long getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+    public long getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+    public LocationEntity() {
+    }
+    public LocationEntity(long id, String name, long latitude, long longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
     
 }
+
